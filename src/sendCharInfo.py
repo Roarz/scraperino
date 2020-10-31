5828a8f0d112969719fa34f71a365305c03ab697
Roarz <rorie.nmit@gmail.com>
2020-10-31 21:57:35 +1300

    tidy up

--- src/sendCharInfo.py
+++ src/sendCharInfo.py
@@ -25,22 +25,10 @@ def check_player(player):
     channel.basic_publish(exchange='', routing_key='hello', body=jsonpickle.encode(lad, unpicklable=False))
     print("[x] Sent '%s'" % lad.name)
 
-def main():
-    channel.queue_declare(queue='hello')
-    check_player("Arkaykos Slayer")
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-            connection.close()
-        except SystemExit:
-            os._exit(0)
-            connection.close()
-
+channel.queue_declare(queue='hello')
+check_player("Arkaykos Slayer")
+connection.close()
