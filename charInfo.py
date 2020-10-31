b43838bd49c6f7c2dcf777cac258176a9892d5ee
Roarz <rorie.nmit@gmail.com>
2020-10-31 16:51:34 +1300

    print character stats and deaths

--- charInfo.py
+++ charInfo.py
@@ -3,24 +3,26 @@ import lxml
 from bs4 import BeautifulSoup
 import pyperclip
 import re
-from functions import list_contains_string, char_data_row
+from functions import list_contains_string, char_data_row, char_deaths_row
+import module1
 
-
-def get_char_stats(characterName): 
+def character_info_table(characterName):
     requestString = "https://www.tibia.com/community/?subtopic=characters&name=%s" % characterName
     source = requests.get(requestString).text
     soup = BeautifulSoup(source,'lxml')
     charContainer = soup.find('div','BoxContent')
-    charInfo = charContainer.find_all(char_data_row)
+    return charContainer
 
+def get_char_stats(charInfo):
+    stats = charInfo.find_all(char_data_row)
     results = {}
-
+    deaths = {}
     character_information = ["Name:","Sex:","Vocation:","Level:","World:","Residence:","Guild","Last Login:"]
 
-    for row in charInfo:
-        #print(x.nextSibling)
+    for row in stats:
         col = row.findChildren("td")
-        if len(col) > 1:
+        #print(row.contents)
+        if len(col) == 2:
             #print(row)
             key = col[0].text
             val = col[1].text
@@ -29,13 +31,28 @@ def get_char_stats(characterName):
                 normalisedKey = re.search(r"(.*):", key)
                 normalisedKey = normalisedKey.group(1)
                 results[normalisedKey] = val
-    
-    return results
 
-def get_deaths(characterName):
-    charInfo = get_char_stats(characterName)
-    print(charInfo["World"])
-    print(charInfo["Name"])
-    print(charInfo["Level"])
+        if row.string == "Character Deaths":
+            deathsRows = row.parent.findChildren("tr")
+            print("info: ", len(deathsRows))
+            for death in deathsRows:
+                cols = death.findChildren("td")
+                if len(cols) == 2:
+                    deaths[cols[0].text] = cols[1].text
 
-get_deaths("ya")
+    character = module1.Character(results["Name"],results["Sex"],results["Vocation"],results["Level"],results["World"],results["Residence"],results["Last Login"])
+    character.deaths = deaths
+
+    return character
+
+def display_character_info(character):
+    for k in char.deaths.values():
+        print(k)
+    print("")
+    print("char: ", char.name, char.level, char.lastLogin, len(char.deaths), "deaths")
+
+
+charName = "Cleitinho Tacaesside"
+charInfo = character_info_table(charName)
+char = get_char_stats(charInfo)
+display_character_info(char)
---
+++ module1.py
@@ -0,0 +1,14 @@
+class Character:
+    def __init__(self, name, sex, vocation, level, world, residence, lastLogin):
+        self.name = name
+        self.sex = sex
+        self.Vocation = vocation
+        self.level = level
+        self.world = world
+        self.residence = residence
+        self.guild = ""
+        self.lastLogin = lastLogin
+        self.deaths = {}
+
+    def add_death(self, level):
+        self.deaths.append(level)
