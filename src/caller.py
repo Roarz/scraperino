9bd31922483b675e9621fb122f41bddbb57510d8
Roarz <rorie.nmit@gmail.com>
2020-10-31 19:53:07 +1300

    printing player info easier

--- src/caller.py
+++ src/caller.py
@@ -1,6 +1,12 @@
 from charInfo import get_player_info_table, parse_char_stats, display_player_info
+import time
 
-playerName = "Cleitinho Tacaesside"
-playerInfo = get_player_info_table(playerName)
-player = parse_char_stats(playerInfo)
-display_player_info(player)
+def print_player_info(playerName):
+    playerInfo = get_player_info_table(playerName)
+    player = parse_char_stats(playerInfo)
+    display_player_info(player)
+    #time.sleep(1)
+
+print_player_info("Excail Oz")
+print_player_info("Azxkhar Te Pekea")
+print_player_info("Young Kodak")
